715a115c5c1461a8bbc30876277a5cf846c05fc7
Chris Garciae <124018793+xxlusubuxx@users.noreply.github.com>
2024-06-21 01:36:15 +0000

    using sandbox to edit

--- main_interface/initiate.js
+++ main_interface/initiate.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
-const port = 3000;
+const path = require("path");
+const port = 2222;
 
 app.use(express.static(path.join(__dirname)));
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-  });
+  console.log(`Server listening at http://localhost:${port}`);
+});
 
-const fs = require('fs')
+const fs = require("fs");
 
-app.get('/api/length', (req, res) => {
-  const length = fs.readdirSync('./slide_backgrounds').length
-  res.json(length)})
+app.get("/api/length", (req, res) => {
+  const length = fs.readdirSync("./slide_backgrounds").length;
+  res.json(length);
+});
